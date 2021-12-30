036d1e4d195ef822e3389acac782f8b239c32cc1
wangshuhan <532846600@qq.com>
2021-12-30 22:37:25 +0800

    c++ virtual

--- cpptest/pure_virtual.cc
+++ cpptest/pure_virtual.cc
@@ -7,13 +7,12 @@ class Base {
 
 };
 // Base::~Base() { std::cout << 0 << std::endl; };
-void Base::Print() { std::cout << 1 << std::endl;};
+void Base::Print() { std::cout << 1 << std::endl; };
 
 class Derived : public Base {
  public:
   virtual void Print() override { Base::Print(); };
 };
-
 
 int main() {
   Derived d;
---
+++ cpptest/virtual.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class Base {
+ public:
+  virtual void Print1() { std::cout << 1 << std::endl; };
+  virtual void Print2() { std::cout << 2 << std::endl; };
+
+};
+
+class Derived : public Base {
+ public:
+  virtual void Print1() { std::cout << 1.1 << std::endl; };
+  void Print2() { std::cout << 2.2 << std::endl; };  // 基类是vir, 子类都是vir的（不论是否有显式地加上vir）
+};
+
+class Derived2 : public Derived {
+ public:
+  void Print2() { std::cout << 2.22 << std::endl; };  // Derived的Print2虽不带vir, 但仍然是vir的，仍然保持多态
+};
+
+int main() {
+  Base *obj = new Derived2;
+  obj->Print1();
+  obj->Print2();
+  return 0;
+}
