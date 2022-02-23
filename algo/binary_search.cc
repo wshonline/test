9d176f423198aea89c5e6c0e9c4e841b99de99c8
wsh <532846600@qq.com>
2022-02-23 17:25:47 +0800

    learning algo

---
+++ algo/binary_search.cc
@@ -0,0 +1,28 @@
+// 整数二分算法
+
+#include <vector>
+
+bool check(int mid) {
+  return true;
+}
+
+// mid属于左半边, 区间划分成[l, mid]和[mid + 1, r]
+int search_1(int l, int r) {
+  int mid = l + r >> 1;
+  if (check(mid)) { r = mid; }
+  else { l = mid + 1; }
+  return l;
+}
+
+// mid属于右半边, 区间划分成[l, mid - 1]和[mid, r]
+int search_2(int l, int r) {
+  int mid = l + r + 1 >> 1;
+  if (check(mid)) { l = mid; }
+  else { r = mid - 1; }
+  return l;
+}
+
+int main() {
+
+  return 0;
+}
